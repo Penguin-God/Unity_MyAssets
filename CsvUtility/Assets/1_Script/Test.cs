79e5f6cd3bd29cee45c238946d46e61272165a69
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-07-04 21:54:10 +0900

    테스트용 클래스, 함수 제작

--- CsvUtility/Assets/1_Script/Test.cs
+++ CsvUtility/Assets/1_Script/Test.cs
@@ -8,6 +8,7 @@ using System.Reflection;
 [Serializable]
 public class TestClass
 {
+    [SerializeField] 
     public Dictionary<int, string> dict = new Dictionary<int, string>();
 
     [SerializeField] List<int> keys = new List<int>();
@@ -23,14 +24,73 @@ public class TestClass
     }
 }
 
+[Serializable]
+public class MasterTest
+{
+    int number;
+    string text;
+    float actualNumber;
+    bool boolean;
+    KeyValuePair<string, int> textNumberPair;
+    KeyValuePair<bool, float> booleanActualNumberPair;
+
+    int[] numberArray;
+    string[] textArray;
+    float[] actualNumberArray;
+    bool[] booleanArray;
+
+    List<int> numberList;
+    List<string> textList;
+    List<float> actualNumberList;
+    List<bool> booleanList;
+
+    Dictionary<int, string> numberByText = new Dictionary<int, string>();
+    Dictionary<float, bool> actualNumberByBoolean = new Dictionary<float, bool>();
+
+    public bool IsSuccess()
+    {
+        return CheckSame(number, 123) || CheckSame(text, "Hello World") || CheckSame(actualNumber, 23.123f) || CheckSame(boolean, true);
+    }
+
+
+    // TODO : 틀렸을 때 정보도 LogError에 띄우기
+    bool CheckSame<T>(T parsingValue, T value) where T : IComparable
+    {
+        if (parsingValue.CompareTo(value) != 0)
+        {
+            Debug.LogError("서로 달라요!!!!!!!!!");
+            return false;
+        }
+
+        return true;
+    }
+}
+
 public class Test : MonoBehaviour
 {
     [SerializeField] TestClass[] testClass;
     [SerializeField] TestClass test;
     [SerializeField] TextAsset asset;
 
-    Dictionary<int, string> a = new Dictionary<int, string>();
-    [ContextMenu("Test")]
+    [ContextMenu("Master Test")]
+    void MasterTest()
+    {
+
+    }
+
+    bool CheckSame<T>(T parsingValue, T value) where T : IComparable
+    {
+        if (parsingValue.CompareTo(value) != 0)
+        {
+            Debug.LogError("서로 달라요!!!!!!!!!");
+            return false;
+        }
+
+        return true;
+    }
+
+
+    //[ContextMenu("Test")]
     void PairTest()
     {
         testClass = CsvUtility.GetEnumerableFromCsv<TestClass>(asset.text).ToArray();
